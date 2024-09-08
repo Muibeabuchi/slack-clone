4633b5beadaa7893dbf175d331a29305dc84208e
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-09-08 23:18:31 +0100

    install jotai + add workspace table to schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -9,23 +9,11 @@ import { v } from "convex/values";
 
 const schema = defineSchema({
   ...authTables,
-  users: defineTable({
-    name: v.optional(v.string()),
-    image: v.optional(v.string()),
-    email: v.optional(v.string()),
-    emailVerificationTime: v.optional(v.number()),
-    phone: v.optional(v.string()),
-    phoneVerificationTime: v.optional(v.number()),
-    isAnonymous: v.optional(v.boolean()),
-    // other "users" fields...
-  })
-    .index("email", ["email"])
-    .index("name", ["name"]),
-  // users: defineTable({
-  //   ...authTables.users,
-  //   name: v.string(),
-  // }),
-  // Your other tables...
+  workspaces: defineTable({
+    name: v.string(),
+    userId: v.id("users"),
+    joincode: v.string(),
+  }),
 });
 
 export default schema;

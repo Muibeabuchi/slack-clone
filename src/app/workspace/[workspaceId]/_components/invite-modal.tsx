51f2fcc6d2a52d2f1fec24b2019107b0714a22a5
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-09-14 17:42:13 +0100

    add confirm dialog to the invite modal + change confirm modal from alert-dialog to dialog

--- src/app/workspace/[workspaceId]/_components/invite-modal.tsx
+++ src/app/workspace/[workspaceId]/_components/invite-modal.tsx
@@ -8,6 +8,7 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useUpdateWorkSpaceJoincode } from "@/features/workspaces/api/use-update-workspace-joicode";
+import { useConfirm } from "@/hooks/use-confirm";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { DialogDescription } from "@radix-ui/react-dialog";
 import { CopyIcon, RefreshCcwIcon } from "lucide-react";
@@ -20,6 +21,11 @@ interface InviteModalProps {
   joinCode: string;
 }
 
+const confirmPrompt = [
+  "Are you sure?",
+  "This will deactivate the current invite code and generate a new one",
+] as const;
+
 export default function InviteModal({
   open,
   setOpen,
@@ -28,9 +34,15 @@ export default function InviteModal({
 }: InviteModalProps) {
   //   const [] = useClipboard();
   const workspaceId = useWorkspaceId();
+  const [ConfirmDialog, confirm] = useConfirm(...confirmPrompt);
   const { mutate: updateJoincode, isPending: updatingJoincode } =
     useUpdateWorkSpaceJoincode();
-  const handleNewcode = () => {
+
+  //  BUG: fix the closing of the dialog whenever the confirm dialog is closed
+  const handleNewcode = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+
     updateJoincode(
       {
         workspaceId,
@@ -56,37 +68,40 @@ export default function InviteModal({
   };
 
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Invite People to {workspaceName}</DialogTitle>
-          <DialogDescription>
-            Use the code below to invite people to your workspace
-          </DialogDescription>
-        </DialogHeader>
-        <div className="flex gap-y-4 justify-center items-center py-10 flex-col">
-          <p className="text-4xl font-bold tracking-widest uppercase">
-            {joinCode}
-          </p>
-          <Button onClick={handleCopy} variant={"ghost"} size="sm">
-            Copy Link
-            <CopyIcon className="size-4 ml-2" />
-          </Button>
-        </div>
-        <div className="flex items-center justify-between w-full">
-          <Button
-            variant="outline"
-            onClick={handleNewcode}
-            disabled={updatingJoincode}
-          >
-            New Code
-            <RefreshCcwIcon className="size-4 ml-2" />
-          </Button>
-          <DialogClose asChild disabled={updatingJoincode}>
-            <Button>Close</Button>
-          </DialogClose>
-        </div>
-      </DialogContent>
-    </Dialog>
+    <>
+      <ConfirmDialog />
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Invite People to {workspaceName}</DialogTitle>
+            <DialogDescription>
+              Use the code below to invite people to your workspace
+            </DialogDescription>
+          </DialogHeader>
+          <div className="flex gap-y-4 justify-center items-center py-10 flex-col">
+            <p className="text-4xl font-bold tracking-widest uppercase">
+              {joinCode}
+            </p>
+            <Button onClick={handleCopy} variant={"ghost"} size="sm">
+              Copy Link
+              <CopyIcon className="size-4 ml-2" />
+            </Button>
+          </div>
+          <div className="flex items-center justify-between w-full">
+            <Button
+              variant="outline"
+              onClick={handleNewcode}
+              disabled={updatingJoincode}
+            >
+              New Code
+              <RefreshCcwIcon className="size-4 ml-2" />
+            </Button>
+            <DialogClose asChild disabled={updatingJoincode}>
+              <Button>Close</Button>
+            </DialogClose>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }

ee9b632e02feac637f184a811b97b9951ff9d498
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-09-15 13:26:21 +0100

    add functionality to redirect from the workspaceId page to an available channel page if found and open create-channel modal if no channel is found

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,22 +1,64 @@
 "use client";
 
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-// import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo } from "react";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { Id } from "../../../../convex/_generated/dataModel";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
+import useGetWorkspace from "@/features/workspaces/api/use-get-workspace";
+import { LoaderIcon, TriangleAlertIcon } from "lucide-react";
 
-// interface WorkspaceIdProps {
-//   params: {
-//     workspaceId: Id<"workspaces">;
-//   };
-// }
+export default function WorkspaceIdPage({
+  params: { workspaceId },
+}: {
+  params: { workspaceId: Id<"workspaces"> };
+}) {
+  const router = useRouter();
+  const { data: workspace, isLoading: workspaceLoading } =
+    useGetWorkspace(workspaceId);
+  const { data: channels, isLoading: loadingChannels } = useGetChannels({
+    workspaceId,
+  });
+  const [openCreateChannel, setOpenCreateChannel] = useCreateChannelModal();
 
-const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
+  // const firstWorkspace = useMemo(() => workspace?._id, [workspace]);
+  const firstChannelId = useMemo(() => channels?.[0]?._id, [channels]);
 
-  // useEffect(()=>{
-  //   if
-  // })
+  useEffect(() => {
+    if (workspaceLoading || loadingChannels || !workspace) return;
 
-  return <div>ID:{workspaceId}</div>;
-};
+    if (firstChannelId) {
+      router.push(`/workspace/${workspaceId}/channel/${firstChannelId}`);
+    } else if (!openCreateChannel) setOpenCreateChannel(true);
+  }, [
+    workspaceLoading,
+    loadingChannels,
+    workspace,
+    router,
+    workspaceId,
+    firstChannelId,
+    openCreateChannel,
+    setOpenCreateChannel,
+  ]);
 
-export default WorkspaceIdPage;
+  if (workspaceLoading || loadingChannels) {
+    return (
+      <div className="flex-1 h-full flex flex-col gap-2 justify-center items-center">
+        <LoaderIcon className="animate-spin size-6 text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!workspace) {
+    return (
+      <div className="flex-1 h-full flex flex-col gap-2 justify-center items-center">
+        <TriangleAlertIcon className="animate-spin size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Workspace not found
+        </span>
+      </div>
+    );
+  }
+
+  return null;
+}

7a7f025a601c0f6fb3a8e0b14f5555f85a582f38
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-08 00:58:52 +0100

    add api to get message by id

--- convex/messages.ts
+++ convex/messages.ts
@@ -123,7 +123,8 @@ export const get = query({
   },
   async handler(ctx, args) {
     const userId = await getAuthUserId(ctx);
-    if (!userId) throw new ConvexError("Unauthorized");
+    // if (!userId) throw new ConvexError("Unauthorized");
+    if (!userId) return null;
 
     let _conversationId = args.conversationId;
 
@@ -209,6 +210,70 @@ export const get = query({
           })
         )
       ).filter((message) => message !== null),
+    };
+  },
+});
+
+export const getById = query({
+  args: { messageId: v.id("messages") },
+  async handler(ctx, args) {
+    const userId = await getAuthUserId(ctx);
+    if (!userId) return null;
+
+    const message = await ctx.db.get(args.messageId);
+    if (!message) return null;
+
+    // ? Only members should load a message
+    const currentMember = await getMember(ctx, message.workspaceId, userId);
+    if (!currentMember) return null;
+
+    const member = await populateMember(ctx, message.memberId);
+    if (!member) return null;
+    const user = await populateUser(ctx, member.userId);
+    if (!user) return null;
+    const reactions = await populateReaction(ctx, message._id);
+
+    const reactionsWithCount = reactions.map((reaction) => {
+      return {
+        ...reaction,
+        count: reactions.filter((r) => {
+          return r.value === reaction.value;
+        }).length,
+      };
+    });
+
+    // deduplicate the reactions
+    const dedupedReactions = reactionsWithCount.reduce(
+      (acc, reaction) => {
+        const existingReaction = acc.find((r) => r.value === reaction.value);
+        if (existingReaction) {
+          existingReaction.memberIds = Array.from(
+            new Set([...existingReaction.memberIds, reaction.memberId])
+          );
+        } else {
+          acc.push({ ...reaction, memberIds: [reaction.memberId] });
+        }
+        return acc;
+      },
+      [] as (Doc<"reactions"> & {
+        count: number;
+        memberIds: Id<"members">[];
+      })[]
+    );
+
+    const reactionWithoutMemberIdProperty = dedupedReactions.map(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      ({ memberId, ...rest }) => rest
+    );
+
+    return {
+      ...message,
+      image: message.image
+        ? await ctx.storage.getUrl(message.image)
+        : undefined,
+      user,
+      member,
+      reactions: reactionWithoutMemberIdProperty,
     };
   },
 });

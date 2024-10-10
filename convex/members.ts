bfe5e7930faef274fbb84e9de5f6579cf6f3bf6f
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-10 19:03:15 +0100

    add api to update-member-role

--- convex/members.ts
+++ convex/members.ts
@@ -1,6 +1,6 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
-import { query, QueryCtx } from "./_generated/server";
-import { v } from "convex/values";
+import { mutation, query, QueryCtx } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 import { Id } from "./_generated/dataModel";
 
 const populateUser = (ctx: QueryCtx, id: Id<"users">) => ctx.db.get(id);
@@ -84,5 +84,49 @@ export const workspaceMembers = query({
     });
 
     return await Promise.all(membersUserIds);
+  },
+});
+
+export const update = mutation({
+  args: { memberId: v.id("members"), workspaceId: v.id("workspaces") },
+  async handler(ctx, args) {
+    // check if the user is authenticated
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new ConvexError("Unauthorized");
+
+    // // populate the user
+    // const user = await populateUser(ctx, userId);
+
+    // does this member exist?
+    const member = await ctx.db.get(args.memberId);
+    if (!member) throw new ConvexError("Member does not exist");
+
+    // check if the member is a part of the workspace
+    const workspaceMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!workspaceMember) throw new ConvexError("Unauthorized");
+
+    // is user a member of workspace?
+    // check if the user is an admin
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!currentMember || currentMember.role !== "admin")
+      throw new ConvexError("Unauthorized");
+
+    await ctx.db.patch(workspaceMember._id, {
+      role: workspaceMember.role === "member" ? "admin" : "member",
+    });
+
+    return workspaceMember._id;
   },
 });

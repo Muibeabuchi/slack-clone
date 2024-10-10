594e468f2b7f00eb0a28fd331c9a80e473d57957
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2024-10-10 19:31:56 +0100

    add api to remove-member from a workspace

--- convex/members.ts
+++ convex/members.ts
@@ -94,9 +94,6 @@ export const update = mutation({
     const userId = await getAuthUserId(ctx);
     if (!userId) throw new ConvexError("Unauthorized");
 
-    // // populate the user
-    // const user = await populateUser(ctx, userId);
-
     // does this member exist?
     const member = await ctx.db.get(args.memberId);
     if (!member) throw new ConvexError("Member does not exist");
@@ -126,6 +123,85 @@ export const update = mutation({
     await ctx.db.patch(workspaceMember._id, {
       role: workspaceMember.role === "member" ? "admin" : "member",
     });
+
+    return workspaceMember._id;
+  },
+});
+
+export const remove = mutation({
+  args: { memberId: v.id("members"), workspaceId: v.id("workspaces") },
+  async handler(ctx, args) {
+    // check if the user is authenticated
+    const userId = await getAuthUserId(ctx);
+    if (!userId) throw new ConvexError("Unauthorized");
+
+    // does this member exist?
+    const member = await ctx.db.get(args.memberId);
+    if (!member) throw new ConvexError("Member does not exist");
+
+    // check if the member is a part of the workspace
+    const workspaceMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", member.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!workspaceMember) throw new ConvexError("Unauthorized");
+
+    // is user a member of workspace?
+    // check if the user is an admin
+    const currentMember = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!currentMember) throw new ConvexError("Unauthorized");
+
+    // ensure admins cannot be removed
+    if (workspaceMember.role === "admin")
+      throw new ConvexError("Admin cannot be removed");
+
+    // ensure member cannot delete themselves
+    const isSelf = workspaceMember._id === currentMember._id;
+    if (isSelf && currentMember.role !== "admin")
+      throw new ConvexError("Cannot remove self if is an admin");
+
+    // clean up all the messages,conversations and reactions this member has made in the workspace
+
+    const [messages, reactions, conversations] = await Promise.all([
+      ctx.db
+        .query("messages")
+        .withIndex("by_member_id", (q) => q.eq("memberId", workspaceMember._id))
+        .collect(),
+      ctx.db
+        .query("reactions")
+        .withIndex("by_member_id", (q) => q.eq("memberId", workspaceMember._id))
+        .collect(),
+      ctx.db
+        .query("conversations")
+        .filter((q) =>
+          q.or(
+            q.eq(q.field("memberOneId"), workspaceMember._id),
+            q.eq(q.field("memberTwoId"), workspaceMember._id)
+          )
+        )
+        .collect(),
+    ]);
+
+    for (const message of messages) {
+      await ctx.db.delete(message._id);
+    }
+    for (const reaction of reactions) {
+      await ctx.db.delete(reaction._id);
+    }
+    for (const conversation of conversations) {
+      await ctx.db.delete(conversation._id);
+    }
+
+    await ctx.db.delete(workspaceMember._id);
 
     return workspaceMember._id;
   },
